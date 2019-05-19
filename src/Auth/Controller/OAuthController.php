5b652c26737328aca708ba693eb05e11c0128ac3
Guilherme Barile <guigouz@gmail.com>
2019-05-19 13:16:43 -0300

    fixup OAuthController header name

--- src/Auth/Controller/OAuthController.php
+++ src/Auth/Controller/OAuthController.php
@@ -35,7 +35,7 @@ class OAuthController extends AuthController
         $config = $this->providers[$id];
 
         $config['redirectUri'] =
-            (empty($_SERVER['X_FORWARDED_PROTO']) ? $_SERVER['REQUEST_SCHEME'] : $_SERVER['X_FORWARDED_PROTO'])
+            (empty($_SERVER['HTTP_X_FORWARDED_PROTO']) ? $_SERVER['REQUEST_SCHEME'] : $_SERVER['HTTP_X_FORWARDED_PROTO'])
             . "://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
 
         $classname = "\\League\\OAuth2\\Client\\Provider\\" . ucfirst($id);
